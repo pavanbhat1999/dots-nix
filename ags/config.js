2849060df72ddab5560af024331aeb780b88258c
pavanbhat1999 <prbhat07@gmail.com>
2023-11-04 10:26:58 +0530

    notification showing again

--- ags/config.js
+++ ags/config.js
@@ -73,7 +73,7 @@ const NotificationCenter = () => Widget.Window({
 const NotificationsPopupWindow = () => Widget.Window({
     name: 'popup-window',
     anchor: ['top'],
-    visible: false,
+    visible: true,
     child: PopupList(),
 });
 
